9569d8a92baff84c720be718a1057d5e9451e6eb
Rynus <rynus2020@gmail.com>
2020-11-04 09:44:28 +0700

    Colour Code Calculator : features : - 4,5,6 band color code calculation - delete 1 band and the whole bands - new layout
    lack :
    - change the band without deleting
    - polishing
    - animation
    - navigation drawer

--- app/src/main/java/com/proto/cocolator/activities/MainActivity.java
+++ app/src/main/java/com/proto/cocolator/activities/MainActivity.java
@@ -1,14 +1,30 @@
 package com.proto.cocolator.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.res.ResourcesCompat;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.StatusBarManager;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 import com.nex3z.togglebuttongroup.SingleSelectToggleGroup;
 import com.proto.cocolator.R;
 import com.proto.cocolator.util.Calculator;
@@ -18,7 +34,7 @@ import com.proto.cocolator.util.RiCol;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, NavigationView.OnNavigationItemSelectedListener {
     public static int C;
     SingleSelectToggleGroup ribbon_mode;
 
@@ -47,11 +63,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     CoManager manager;
     TextView result;
     Drawable res;
+    DrawerLayout drawer;
+    boolean slideState;
+
+    public MainActivity() {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        drawer = findViewById(R.id.drawer);
+        NavigationView nav = findViewById(R.id.nav_view);
+        nav.setNavigationItemSelectedListener(this);
 
 
         ribbon_mode = findViewById(R.id.ribbon_group);
@@ -91,7 +119,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         manager = new CoManager(res, C, ribbon_mode, first_ribbon, second_ribbon, third_ribbon, fourth_ribbon, fifth_ribbon, sixth_ribbon, colButton, ribbons);
         manager.ribbonSetUp();
 
-
         black.setOnClickListener(this);
         brown.setOnClickListener(this);
         red.setOnClickListener(this);
@@ -111,54 +138,66 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         int ribbonColour = 0;
-
+        final Animation bouncing = AnimationUtils.loadAnimation(this, R.anim.bounce);
         short code = 0;
         switch (v.getId()) {
             case R.id.black:
+                black.startAnimation(bouncing);
                 ribbonColour = RiCol.BLACK;
                 code = 0;
                 break;
             case R.id.brown:
+                brown.startAnimation(bouncing);
                 ribbonColour = RiCol.BROWN;
                 code = 1;
                 break;
             case R.id.red:
+                red.startAnimation(bouncing);
                 ribbonColour = RiCol.RED;
                 code = 2;
                 break;
             case R.id.orange:
+                orange.startAnimation(bouncing);
                 ribbonColour = RiCol.ORANGE;
                 code = 3;
                 break;
             case R.id.yellow:
+                yellow.startAnimation(bouncing);
                 code = 4;
                 ribbonColour = RiCol.YELLOW;
                 break;
             case R.id.green:
+                green.startAnimation(bouncing);
                 code = 5;
                 ribbonColour = RiCol.GREEN;
                 break;
             case R.id.blue:
+                blue.startAnimation(bouncing);
                 code = 6;
                 ribbonColour = RiCol.BLUE;
                 break;
             case R.id.purple:
+                purple.startAnimation(bouncing);
                 code = 7;
                 ribbonColour = RiCol.PURPLE;
                 break;
             case R.id.gray:
+                gray.startAnimation(bouncing);
                 code = 8;
                 ribbonColour = RiCol.GRAY;
                 break;
             case R.id.white:
+                white.startAnimation(bouncing);
                 code = 9;
                 ribbonColour = RiCol.WHITE;
                 break;
             case R.id.silver:
+                silver.startAnimation(bouncing);
                 code = 10;
                 ribbonColour = RiCol.SILVER;
                 break;
             case R.id.gold:
+                gold.startAnimation(bouncing);
                 code = 11;
                 ribbonColour = RiCol.GOLD;
                 break;
@@ -182,11 +221,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     break;
             }
         }
-
-        silver.setClickable((C < (ribbons.size() - 2)));
-        gold.setClickable((C < (ribbons.size() - 2)));
-
         C++;
+
+        boolean clickable = (C >= ribbons.size() - 1 || C > 3) ? true : false;
+
+
+        silver.setClickable(clickable);
+        gold.setClickable(clickable);
+
+
+        Log.d("NUPIE", "onClick: \n"
+                + "C = " + C + "\n"
+                + "clickable = " + clickable + "\n"
+                + "r size = " + ribbons.size()
+                + "r size -2 = " + (ribbons.size() - 2));
     }
 
     public void clear(View view) {
@@ -201,5 +249,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         C = (C == 0) ? 0 : C - 1;
         C = Math.abs(C % ribbons.size());
         ribbons.get(C).setBackground(res);
+    }
+
+    public void open(View view) {
+        drawer.openDrawer(GravityCompat.START);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.credit:
+                Toast.makeText(this, "hei", Toast.LENGTH_SHORT).show();
+
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setView(getLayoutInflater().inflate(R.layout.fragment_credit, null));
+                builder.show();
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+
+        return false;
     }
 }
